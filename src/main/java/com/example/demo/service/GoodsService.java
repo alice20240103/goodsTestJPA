208a324858fcc8de86e219f309957d9eb1f57ba5
alice20240103 <alice20240103@gmail.com>
2024-03-19 14:18:45 +0900

    first commit

---
+++ src/main/java/com/example/demo/service/GoodsService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.GoodsDAO;
+import com.example.demo.entity.GoodsVO;
+
+import lombok.Setter;
+
+@Service
+@Setter
+public class GoodsService {
+	@Autowired
+	private GoodsDAO dao;
+	
+	public List<GoodsVO> findAll(){
+		return dao.findAll();
+	}
+}
+
+
+
+
+
+
